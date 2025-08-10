4ca5171b630c0e1c3f66346ce1d84f26c1e293b7
Enzo Granado <enzogranado@gmail.com>
2025-08-10 13:02:57 -0300

    feat: Add JavaScript Files

--- dice/index.js
+++ dice/index.js
@@ -18,7 +18,7 @@ else if(randomNumber==5){
 else if(randomNumber==6){
     document.querySelector(".img1").setAttribute("src", "./images/dice6.png")   
 }
-// lkkkkkkkkkkkkk
+// 
 var randomNumber2 = Math.ceil((Math.random())*6);
 if(randomNumber2==1){
     document.querySelector(".img2").setAttribute("src", "./images/dice1.png")
